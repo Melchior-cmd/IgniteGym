5dd7f8ee5c3a10c68e75032b2ba0d289c1c4ebd6
Lucas Melchior <lucas.melchior@dpu.def.br>
2025-05-22 01:24:01 -0300

    Criando o componente Header da aplicação e instalando a dep lucide-react-native

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,11 +1,10 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { HomeHeader } from "@components/HomeHeader";
+import { Center, Text, VStack } from "@gluestack-ui/themed";
 
 export function Home() {
   return (
-    <Center flex={1} bg="$background">
-      <Text fontSize={20} color="$white">
-        Home
-      </Text>
-    </Center>
+    <VStack flex={1} bg="$background">
+      <HomeHeader />
+    </VStack>
   );
 }
---
+++ src/components/UserPhoto.tsx
@@ -0,0 +1,16 @@
+import { Center, Image } from "@gluestack-ui/themed";
+import { Component, ComponentProps } from "react";
+
+type Props = ComponentProps<typeof Image>;
+
+export function UserPhoto({ ...rest }: Props) {
+  return (
+    <Image
+      rounded="$full"
+      borderWidth="$2"
+      borderColor="$gray400"
+      backgroundColor="$gray500"
+      {...rest}
+    />
+  );
+}
